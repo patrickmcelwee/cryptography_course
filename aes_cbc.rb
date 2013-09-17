7d48660d3ba2ff4bc0581ab0921de9470204bdfe
Patrick McElwee <pmm21@duke.edu>
2013-09-16 21:24:17 -0400

    Implement AES in CTR mode
    Get to reuse most code, for now in an inheritance class.

--- aes_cbc.rb
+++ aes_cbc.rb
@@ -1,4 +1,3 @@
-#encoding: US-ASCII
 require 'openssl'
 require 'securerandom'
 require_relative 'lib/xorable'
@@ -7,13 +6,24 @@ class String
   include Xorable
 end
 
-class AesCbc
+class AesCipher
   def initialize(initial_string, input_encoding: :ascii,
                  output_encoding: :ascii,
                  key:  ->{raise "needs a key"}.call)
     @initial_string = (normalize_input initial_string, input_encoding).dup
     @output_encoding = output_encoding
     @key = normalize_input key, :hex
+  end
+
+  def normalize_input(input, input_encoding)
+    case input_encoding
+    when :ascii
+      input
+    when :hex
+      input.scan(/../).map { |x| x.hex }.pack('C*')
+    else
+      raise "Not a known encoding: #{input_encoding}"
+    end
   end
 
   def encrypt
@@ -26,6 +36,98 @@ class AesCbc
 
   private
   attr_reader :initial_string, :output_encoding, :key
+  
+  def normalize_output(output, output_encoding)
+    case output_encoding
+    when :ascii
+      output
+    when :hex
+      output.unpack('H*').first
+    else
+      raise "Not a known encoding: #{input_encoding}"
+    end
+  end
+
+  def pad(blocks)
+    padded = false
+    blocks.each do |block|
+      if block.size < 16
+        pad = 16 - block.size
+        if pad < 10
+          pad.times { block << pad.to_s }
+        else
+          (pad-1).times { block << (pad-10).to_s }
+          block << "|"
+        end
+        padded = true
+      end
+    end
+    blocks << "0" * 16 unless padded
+  end
+
+  def encrypt_block(block)
+    encrypter = OpenSSL::Cipher::AES.new(128, 'ECB')
+    encrypter.encrypt
+    encrypter.key = key
+    result = encrypter.update(block)
+    result
+  end
+
+  def unpad(decrypted_message)
+    padding = case decrypted_message[-1]
+              when '0'
+                16
+              when '|'
+                decrypted_message[-2].to_i + 10
+              else
+                decrypted_message[-1].to_i
+              end
+    decrypted_message.slice!(-padding..-1)
+  end
+end
+
+class AesCtr < AesCipher
+  private
+
+  # Note: These two methods basically the same, except how they generate
+  # nonce (one generates and other just receives)
+  def encrypted_string
+    blocks = initial_string.chars.each_slice(16).map(&:join)
+
+    nonce = SecureRandom.random_bytes(8)
+    counter = [0] * 8
+    counter_bytes = counter.pack('C*')
+    encryption = nonce + counter_bytes
+
+    blocks.each do |block|
+      cipher_block = block ^ encrypt_block(nonce + counter_bytes)
+      encryption << cipher_block
+      counter[-1] += 1
+      counter_bytes = counter.pack('C*')
+    end
+
+    encryption
+  end
+
+  def decrypted_string
+    iv = initial_string.slice!(0..15)
+    decryption = ''
+
+    blocks = initial_string.chars.each_slice(16).map(&:join)
+    blocks.each do |block|
+      message_block = block ^ encrypt_block(iv)
+      decryption << message_block
+      iv_counter = iv.bytes
+      iv_counter[-1] += 1
+      iv = iv_counter.pack('C*')
+    end
+
+    decryption
+  end
+end
+
+class AesCbc < AesCipher
+  private
 
   def decrypted_string
     iv = initial_string.slice!(0..15)
@@ -63,64 +165,5 @@ class AesCbc
     decrypter.padding = 0
     result = decrypter.update(block)
     result
-  end
-
-  def encrypt_block(block)
-    encrypter = OpenSSL::Cipher::AES.new(128, 'ECB')
-    encrypter.encrypt
-    encrypter.key = key
-    result = encrypter.update(block)
-    result
-  end
-
-  def pad(blocks)
-    padded = false
-    blocks.each do |block|
-      if block.size < 16
-        pad = 16 - block.size
-        if pad < 10
-          pad.times { block << pad.to_s }
-        else
-          (pad-1).times { block << (pad-10).to_s }
-          block << "|"
-        end
-        padded = true
-      end
-    end
-    blocks << "0" * 16 unless padded
-  end
-
-  def unpad(decrypted_message)
-    padding = case decrypted_message[-1]
-              when '0'
-                16
-              when '|'
-                decrypted_message[-2].to_i + 10
-              else
-                decrypted_message[-1].to_i
-              end
-    decrypted_message.slice!(-padding..-1)
-  end
-
-  def normalize_input(input, input_encoding)
-    case input_encoding
-    when :ascii
-      input
-    when :hex
-      input.scan(/../).map { |x| x.hex }.pack('C*')
-    else
-      raise "Not a known encoding: #{input_encoding}"
-    end
-  end
-
-  def normalize_output(output, output_encoding)
-    case output_encoding
-    when :ascii
-      output
-    when :hex
-      output.unpack('H*').first
-    else
-      raise "Not a known encoding: #{input_encoding}"
-    end
   end
 end
--- spec/aes_cbc_spec.rb
+++ spec/aes_cbc_spec.rb
@@ -1,5 +1,43 @@
-#encoding: US-ASCII
 require_relative '../aes_cbc'
+
+describe AesCtr do
+  it "works for a short message" do
+    message = "This is me."
+    key = SecureRandom.random_bytes(16).unpack('H*').first
+    encrypter = AesCtr.new(message, key: key)
+    cipher = encrypter.encrypt
+
+    decrypter = AesCtr.new(cipher, key: key)
+    expect(decrypter.decrypt).to eq(message)
+  end
+
+  it "encrypts and decrypts a message with hex encoding" do
+    plain_text_message = "This is a top-secret message!"
+    message = plain_text_message.unpack('H*').first
+    key = '140b41b22a29beb4061bda66b6747e14'
+    encrypter = AesCtr.new(message, input_encoding: :hex, key: key)
+    cipher = encrypter.encrypt
+    
+    expect(cipher.size).to eq 48
+
+    plain_text_decrypter = AesCtr.new(cipher, key: key)
+    expect(plain_text_decrypter.decrypt).to eq(plain_text_message)
+
+    decrypter = AesCtr.new(cipher, output_encoding: :hex, key: key)
+    expect(decrypter.decrypt).to eq(message)
+  end
+
+  # Note, still will not work for blocks < 128 blocks
+  it "works for a long message" do
+    message = "Something ridiculously long so that I can test out the functioning of this crude, but passable reconstruction of a CBC for the purposes of learning cryptography. Cryptography is like the rifle - a military technology available to the masses and thus facilitating a certain level of democracy."
+    key = SecureRandom.random_bytes(16).unpack('H*').first
+    encrypter = AesCbc.new(message, key: key)
+    cipher = encrypter.encrypt
+
+    decrypter = AesCbc.new(cipher, key: key)
+    expect(decrypter.decrypt).to eq(message)
+  end
+end
 
 describe AesCbc do
   it "encrypts and decrypts a message with hex encoding" do
